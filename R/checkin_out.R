7ca23bcf7f3394e6ac1b084ce9af3d211690eb0f
neilcampbelll <neil.campbelll@gmail.com>
2025-04-12 12:50:57 +0200

    updated read in/out functions with JWT

--- R/checkin_out.R
+++ R/checkin_out.R
@@ -3,7 +3,7 @@
 #' Check out the file first to prevent other users from changing it.
 #' Then, check it back in after you've made your changes.
 #'
-#' @param file a name of a file on sharepoint.
+#' @param file a name of a file on SharePoint.
 #' @param directory a directory name.
 #' @param site a SharePoint site name, e.g. '/ExpertGroups/WGNSSK'.
 #' @param site_collection a SharePoint site collection, will almost exclusively
@@ -21,12 +21,17 @@
 #' @rdname checkinout
 
 spcheckout <- function(file, directory, site, site_collection) {
-
+  if (missing(site_collection))
+    site_collection <- getOption("icesSharePoint.site_collection")
+  
+  if (missing(site))
+    site <- getOption("icesSharePoint.site")
+  
   # create service
   service <- sprintf("GetFileByServerRelativeUrl('%s/%s/%s')/checkout",
                      site, directory, file)
   uri <- paste0(site_collection, site, "/_api/web/", utils::URLencode(service))
-
+  
   # call the service
   sppost(uri,
          httr::add_headers("X-RequestDigest" = SP_fdv(site, site_collection)))
@@ -36,12 +41,17 @@ spcheckout <- function(file, directory, site, site_collection) {
 #' @export
 #' @rdname checkinout
 spundocheckout <- function(file, directory, site, site_collection) {
-
+  if (missing(site_collection))
+    site_collection <- getOption("icesSharePoint.site_collection")
+  
+  if (missing(site))
+    site <- getOption("icesSharePoint.site")
+  
   # create service
   service <- sprintf("GetFileByServerRelativeUrl('%s/%s/%s')/undocheckout",
                      site, directory, file)
   uri <- paste0(site_collection, site, "/_api/web/", utils::URLencode(service))
-
+  
   # call the service
   sppost(uri,
          httr::add_headers("X-RequestDigest" = SP_fdv(site, site_collection)))
@@ -51,12 +61,17 @@ spundocheckout <- function(file, directory, site, site_collection) {
 #' @export
 #' @rdname checkinout
 spcheckin <- function(comment, file, directory, site, site_collection) {
-
+  if (missing(site_collection))
+    site_collection <- getOption("icesSharePoint.site_collection")
+  
+  if (missing(site))
+    site <- getOption("icesSharePoint.site")
+  
   # create service
   service <- sprintf("GetFileByServerRelativeUrl('%s/%s/%s')/CheckIn(comment='%s',checkintype=0)",
                      site, directory, file, comment)
   uri <- paste0(site_collection, site, "/_api/web/", utils::URLencode(service))
-
+  
   # call the service
   sppost(uri,
          httr::add_headers("X-RequestDigest" = SP_fdv(site, site_collection)))
