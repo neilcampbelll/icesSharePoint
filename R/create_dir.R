72512e31caa61e102e2d3822cea27c036ff30274
neilcampbelll <neil.campbelll@gmail.com>
2025-04-12 12:51:38 +0200

    updated to use JWT

--- R/create_dir.R
+++ R/create_dir.R
@@ -1,11 +1,12 @@
 #' Create and delete folders
 #'
-#' Create and delete folders on the SharePoint system.
+#' Create and delete folders on the SharePoint system using JWT authentication.
 #'
-#' @param folder a name of a folder on sharepoint.
+#' @param folder a name of a folder on SharePoint.
 #' @param directory a directory name.
 #' @param site a SharePoint site name, e.g. '/ExpertGroups/WGNSSK'.
 #' @param site_collection a SharePoint site collection, will almost exclusively
+#'             be 'https://community.ices.dk'.
 #'
 #' @return
 #' invisibly returns the response from the SharePoint server
@@ -17,14 +18,19 @@
 #' @rdname dircreate
 
 spdir.create <- function(folder, directory, site, site_collection) {
-
+  if (missing(site_collection))
+    site_collection <- getOption("icesSharePoint.site_collection")
+  
+  if (missing(site))
+    site <- getOption("icesSharePoint.site")
+  
   # create service
   uri <- paste0(site_collection, site, "/_api/web/folders")
-
-  # build POST body (i.e. the file contents)
+  
+  # build POST body (i.e. the folder metadata)
   body <- sprintf("{ '__metadata': { 'type': 'SP.Folder' }, 'ServerRelativeUrl': '%s/%s'}",
                   directory, folder)
-
+  
   # send request
   sppost(uri,
          body = body,
@@ -35,13 +41,17 @@ spdir.create <- function(folder, directory, site, site_collection) {
 
 #' @export
 #' @rdname dircreate
-
 spdir.delete <- function(folder, directory, site, site_collection) {
-
+  if (missing(site_collection))
+    site_collection <- getOption("icesSharePoint.site_collection")
+  
+  if (missing(site))
+    site <- getOption("icesSharePoint.site")
+  
   # create service
   service <- sprintf("GetFolderByServerRelativeUrl('%s/%s')", directory, folder)
   uri <- paste0(site_collection, site, "/_api/web/", utils::URLencode(service))
-
+  
   # send request
   sppost(uri,
          httr::add_headers("X-RequestDigest" = SP_fdv(site, site_collection),
